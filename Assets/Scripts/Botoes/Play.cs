ecd8f458a736105c5c00cd6e5dd0b2cf42170bcd
gleidsoncosta <gmendes.costa@hotmail.com>
2021-01-31 14:30:17 -0300

    menu completo

---
+++ Assets/Scripts/Botoes/Play.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Play : MonoBehaviour
+{
+    public string scenename;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openScene(){
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void exit(){
+         Application.Quit();
+    }
+    
+}
